2f47ac2d4be145fb0f4d17243a6adbc347865c11
kojiyamauchi <all.the.small.things@me.com>
2018-05-13 04:18:25 +0900

    added Hot Reload on core.js

--- docs/base/core.js
+++ docs/base/core.js
@@ -11,16 +11,33 @@ import ReactDOM from 'react-dom'
 // Import Component.
 import App from '@/apps/App'
 
-// Import BrowserRouter on React Router DOM.
-import { BrowserRouter } from 'react-router-dom'
-
 // Import Global Styles.
 import '@/styles/StyleGlobal'
 
-// Render on index.html
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('app')
-)
+// Import AppContainer on React Hot Loader.
+import { AppContainer } from 'react-hot-loader'
+
+// Import BrowserRouter on React Router DOM.
+import { BrowserRouter } from 'react-router-dom'
+
+// Render Function.
+const render = component => {
+  ReactDOM.render(
+    <AppContainer>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppContainer>,
+    document.getElementById('app')
+  )
+}
+
+// Render Default.
+render(App)
+
+// If Code Modified, Browser Hot Reload.
+if (module.hot) {
+  module.hot.accept(App, () => {
+    render(App)
+  })
+}
