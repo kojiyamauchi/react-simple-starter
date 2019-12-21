f761ba9a762b18ac036d1114b285eb0a78188ef4
kojiyamauchi <all.the.small.things@me.com>
2019-12-22 00:50:26 +0900

    delete types dir.

--- types/declaration/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.svg'
-declare module '*.jpg'
-declare module '*.png'
-declare module '*.gif'
-declare module '*.json' {
-  const value: any
-  export default value
-}
--- types/enums/index.ts
+++
@@ -1,3 +0,0 @@
-enum AddEnumName {
-  // Add Member.
-}
