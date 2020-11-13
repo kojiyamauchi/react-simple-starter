37a6182e3cebf287e96bd2fb24b9f9b530c33087
kojiyamauchi <all.the.small.things@me.com>
2020-11-13 17:54:55 +0900

    update StyleConstants.ts

--- resource/apps/styles/StyleConstants.ts
+++ resource/apps/styles/StyleConstants.ts
@@ -15,7 +15,7 @@ export const BREAK_POINT_WIDTH = {
 
 // Set Global Font.
 export const GLOBAL_FONT =
-  '"Source Sans Pro", "Noto Sans Japanese", -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Hiragino Sans", "Hiragino Kaku Gothic ProN", "游ゴシック Medium", meiryo, sans-serif'
+  '"Source Sans Pro", "Noto Sans JP", -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Hiragino Sans", "Hiragino Kaku Gothic ProN", "游ゴシック Medium", meiryo, sans-serif'
 
 // Color.
 export const GLOBAL_COLOR = '#777'
