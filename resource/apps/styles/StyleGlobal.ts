cc0171ff5949023fefbb402b2e0099625ee7cc76
Koji Yamauchi <all.the.small.things@me.com>
2020-11-13 18:06:04 +0900

    Revert "Update Default Style. 🎨"

--- resource/apps/styles/StyleGlobal.ts
+++ resource/apps/styles/StyleGlobal.ts
@@ -9,108 +9,44 @@ import { GLOBAL_FONT, GLOBAL_COLOR } from '@/styles/StyleConstants'
 
 // eslint-disable-next-line import/prefer-default-export
 export const GlobalStyle = createGlobalStyle`
-  @import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,900&display=swap);
-  @import url(https://fonts.googleapis.com/css?family=Lobster&display=swap);
-  @import url(https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap);
+  @import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,900);
+  @import url(https://fonts.googleapis.com/css?family=Lobster);
+  @import url(https://fonts.googleapis.com/earlyaccess/notosansjapanese.css);
 
-  a,
-  address,
-  article,
-  aside,
-  audio,
-  blockquote,
-  button,
-  body,
-  canvas,
-  caption,
-  code,
-  dd,
-  div,
-  dl,
-  dt,
-  footer,
-  form,
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  header,
-  hgroup,
-  html,
-  iframe,
-  img,
-  input,
-  label,
-  li,
-  mark,
-  menu,
-  nav,
-  ol,
-  output,
-  p,
-  pre,
-  ruby,
-  section,
-  select,
-  span,
-  summary,
-  table,
-  tbody,
-  td,
-  textarea,
-  th,
-  time,
-  tr,
-  ul,
-  video {
-    padding: 0;
-    margin: 0;
+  html, body, div, span, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, abbr, address, cite, code, del, dfn, em, img, ins, kbd, q, samp, small, strong, sub, sup, var, b, i, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, figcaption, figure, footer, header, hgroup, menu, nav, section, summary, time, mark, audio, video {
+    font-family: ${GLOBAL_FONT};
+    color: ${GLOBAL_COLOR};
     font-size: 100%;
-    font-style: normal;
     font-weight: normal;
+    font-style: normal;
+    letter-spacing: 0;
+    line-height: 1;
+    margin: 0;
+    padding: 0;
     vertical-align: baseline;
     border: 0;
-  }
-
-  * {
+    -webkit-text-size-adjust: 100%;
+    -ms-text-size-adjust: 100%;
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
-    -ms-box-sizing: border-box;
     box-sizing: border-box;
-    overflow-wrap: break-word;
+    -ms-box-sizing: border-box;
   }
 
   html {
     width: 100vw;
+    height: 100%;
     font-size: 62.5%;
   }
 
   body {
-    position: relative;
-    display: flex;
-    flex-direction: column;
     width: 100%;
-    min-height: 100vh;
-    padding: 50px 0 20px;
-    overflow-x: hidden;
-    font-family: ${GLOBAL_FONT};
-    line-height: 1;
-    color: ${GLOBAL_COLOR};
-    letter-spacing: 0;
     background: #fff;
+    position: relative;
+    overflow-x: hidden;
     -webkit-overflow-scrolling: touch;
     -webkit-text-size-adjust: 100%;
     -ms-text-size-adjust: 100%;
-
-    @supports (-webkit-touch-callout: none) {
-      /* height: -webkit-fill-available; */
-    }
-
-    @media all and (-ms-high-contrast: none) {
-      /* display: block; */
-    }
   }
 
   article,
@@ -132,13 +68,16 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   a {
-    color: inherit;
+    transition: opacity 0.5s;
     text-decoration: none;
-    cursor: pointer;
+    opacity: 1;
+    color: inherit;
     background: transparent;
   }
 
   a:hover {
+    transition: opacity 0.5s;
+    opacity: 0.5;
     color: inherit;
   }
 
@@ -146,10 +85,12 @@ export const GlobalStyle = createGlobalStyle`
   a:hover,
   a:link,
   a:visited {
-    color: inherit;
     text-decoration: none;
+    color: inherit;
     outline: none;
   }
+
+  a.current {}
 
   img {
     vertical-align: middle;
@@ -167,32 +108,19 @@ export const GlobalStyle = createGlobalStyle`
     border-collapse: collapse;
   }
 
-  button {
-    cursor: pointer;
-  }
-
   input[type="button"],
   input[type="submit"],
   input[type="text"],
   select,
   button {
-    background-color: transparent;
     border-radius: 0;
     -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-
-    &:focus {
-      outline: none;
-    }
   }
 
   textarea {
     resize: vertical;
     border-radius: 0;
     -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
   }
 
   ::selection {
@@ -204,26 +132,18 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   @media screen and (-webkit-min-device-pixel-ratio: 0) {
-    /* Safari Hack */
-
     * {}
   }
 
   @media all and (-webkit-min-device-pixel-ratio: 0) and (min-resolution: .001dpcm) {
-    /* Chrome Hack */
-
     * {}
   }
 
   @-moz-document url-prefix() {
-    /* FireFox Hack */
-
     * {}
   }
 
   @media all and (-ms-high-contrast: none) {
-    /* IE Hack */
-
     * {}
   }
 `
