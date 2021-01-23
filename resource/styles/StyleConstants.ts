c1d50a0a0fa67df4e7c213ab04bbf0ca4741895b
kojiyamauchi <all.the.small.things@me.com>
2021-01-23 15:42:25 +0900

    update styles.

---
+++ resource/styles/StyleConstants.ts
@@ -0,0 +1,21 @@
+/*
+  Constants for Style.
+*/
+
+// Break Point Width.
+export const BREAK_POINT_WIDTH = {
+  smallMobile: 325,
+  mobile: 480,
+  largeMobile: 768,
+  tablet: 1024,
+  desktop: 1170,
+  largeDesktop: 1392,
+  extraLargeDesktop: 1920
+} as const
+
+// Set Global Font.
+export const GLOBAL_FONT =
+  '"Source Sans Pro", "Noto Sans JP", -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Hiragino Sans", "Hiragino Kaku Gothic ProN", "游ゴシック Medium", meiryo, sans-serif'
+
+// Color.
+export const GLOBAL_COLOR = '#777'
