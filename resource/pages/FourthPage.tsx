e51558a68766dadc104c36172004744f6953b18b
kojiyamauchi <all.the.small.things@me.com>
2021-04-10 22:57:43 +0900

    update pages components.

--- resource/pages/FourthPage.tsx
+++ resource/pages/FourthPage.tsx
@@ -3,8 +3,8 @@
 */
 
 import { useLocation } from 'react-router-dom'
-import { Layout } from '@/components/Layout'
-import { SEO } from '@/components/Seo'
+import { Layout } from '@/components/Layout/'
+import { SEO } from '@/components/Seo/'
 import { PageHeading } from '@/components/Presentations/PageHeading'
 import styled from 'styled-components'
 import { PagesStyle } from '@/styles/StyleMixins'
@@ -17,7 +17,7 @@ const FourthPageComponent: React.VFC<Props> = ({ className }): JSX.Element => {
   const location = useLocation()
 
   return (
-    <Layout pages="fourth" fixedLayout={true}>
+    <Layout pages="fourth" chooseLayout="fixedLayout">
       <SEO title="Fourth Page" description="This is Fourth Page" locationPath={location.pathname} />
       <div className={className}>
         <PageHeading letter="Fourth" className="fourth-page page-heading" />
