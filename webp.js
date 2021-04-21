a1412d037bb412a1fae1efc96106001a0c3d0467
kojiyamauchi <all.the.small.things@me.com>
2021-04-21 23:17:19 +0900

    update webp.js

--- webp.js
+++ webp.js
@@ -2,6 +2,10 @@ const imagemin = require('imagemin')
 const webp = require('imagemin-webp')
 const inDir = 'resource/materials/toWebps/*.{png,jpg,jpeg,gif}'
 const outDir = 'resource/materials/images/'
+const logFontBold = '\x1b[1m'
+const logFontColorCyan = '\u001b[36m'
+const logFontColorMagenta = '\u001b[35m'
+const logFontReset = '\x1b[0m'
 
 ;(async () => {
   const dataObjArr = await imagemin([inDir], {
@@ -14,6 +18,8 @@ const outDir = 'resource/materials/images/'
   })
 
   const dataArr = dataObjArr.map((info) => info.destinationPath.replace(outDir, ''))
-  dataArr.length === 0 ? console.log('No Convert to Webps') : console.log('Converted to Webps List Below,\n', dataArr)
+  dataArr.length === 0
+    ? console.log(`😎 ${logFontBold}${logFontColorMagenta}No Convert to Webps.${logFontReset}`)
+    : console.log(`👍 ${logFontBold}${logFontColorCyan}Converted to Webps List Below,${logFontReset}\n`, dataArr)
   //-> [{data: <Buffer 89 50 4e …>, destinationPath: 'build/images/foo.jpg'}, …]
 })()
