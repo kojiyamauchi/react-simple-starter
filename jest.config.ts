886652ae4ee13cb28d74f9e6e5ed0db40be2d74c
kojiyamauchi <all.the.small.things@me.com>
2021-05-25 19:24:10 +0900

    add & update each config files.

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleNameMapper: {
+    '^@/(.+)': '<rootDir>/resource/$1',
+    '^~/(.+)': '<rootDir>/$1'
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '\\.(webp|svg|png|jpg|jpeg|gif)$': '<rootDir>/jestFileTransformer.js'
+  },
+  setupFilesAfterEnv: ['<rootDir>/testingLibrary.config.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json'
+    },
+    webpackEnvMode: 'test'
+  },
+  testMatch: [
+    '**/resource/tests/**/*.test.+(ts|tsx|js|jsx)',
+    '**/resource/components/**/*.test.+(ts|tsx|js|jsx)',
+    '**/resource/hooks/**/*.test.+(ts|tsx|js|jsx)'
+  ]
+}
+
+export default config
