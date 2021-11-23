72e3b3470a4f72df7132f8c0e95cfc9ef46c015d
kojiyamauchi <all.the.small.things@me.com>
2021-11-23 18:40:48 +0900

    update router.(react-router-dom v5 -> v6)

--- resource/router/index.tsx
+++ resource/router/index.tsx
@@ -2,7 +2,7 @@
   Router.
 */
 
-import { Route, Switch } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { PrimaryPage } from '@/pages/PrimaryPage'
 import { SecondaryPage } from '@/pages/SecondaryPage'
 import { ThirdPage } from '@/pages/ThirdPage'
@@ -10,12 +10,12 @@ import { FourthPage } from '@/pages/FourthPage'
 import { pathPrefix } from '~/base.config'
 
 const RouterComponent: React.VFC = (): JSX.Element => (
-  <Switch>
-    <Route exact path={`${pathPrefix()}/`} component={PrimaryPage} />
-    <Route exact path={`${pathPrefix()}/second`} component={SecondaryPage} />
-    <Route exact path={`${pathPrefix()}/third`} component={ThirdPage} />
-    <Route exact path={`${pathPrefix()}/fourth`} component={FourthPage} />
-  </Switch>
+  <Routes>
+    <Route path={`${pathPrefix()}/`} element={<PrimaryPage />} />
+    <Route path={`${pathPrefix()}/second`} element={<SecondaryPage />} />
+    <Route path={`${pathPrefix()}/third`} element={<ThirdPage />} />
+    <Route path={`${pathPrefix()}/fourth`} element={<FourthPage />} />
+  </Routes>
 )
 
 export const Router = RouterComponent
